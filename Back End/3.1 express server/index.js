a05ebc076c3744b39c041a39a47b533174b72d94
mohan6458 <mohanakrishnan85670@gmail.com>
2024-02-12 20:30:23 +0000

    commit

--- Back End/3.1 express server/index.js
+++ Back End/3.1 express server/index.js
@@ -2,6 +2,6 @@ import express from "express";
 const app = express();
 const port = 3000;
 
-app.listen(port,() =>{
-    console.log(`Server runing on port ${port}.`);
-})
+app.listen(port, () => {
+  console.log(`Server running on port ${port}.`);
+});
